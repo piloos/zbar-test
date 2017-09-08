78e4fa3bfb425b516af5f7b3213d84193f7f4820
Lode Cools <lode.cools@barco.com>
2017-09-08 14:55:00 +0200

    First attempt to focus only on QR code region

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <QSize>
 #include <QElapsedTimer>
 #include <QImage>
+#include <QRect>
+#include <QMargins>
 
 #include <iostream>
 #include <fstream>
@@ -12,7 +14,21 @@
 
 using namespace std;
 
-pair<char*, int> read_file(string filename)
+static QRect get_area(const zbar::Symbol& symbol)
+{
+    QPoint start((*symbol.point_begin()).x, (*symbol.point_begin()).y);
+    QRect rect(start.x(), start.y(), 0, 0);
+    for (zbar::Symbol::PointIterator it = ++symbol.point_begin(); it != symbol.point_end(); ++it) {
+        QPoint point((*it).x, (*it).y);
+        if (point.x() < rect.left()) rect.setLeft(point.x());
+        if (point.x() > rect.right()) rect.setRight(point.x());
+        if (point.y() < rect.top()) rect.setTop(point.y());
+        if (point.y() > rect.bottom()) rect.setBottom(point.y());
+    }
+    return rect;
+}
+
+static pair<char*, int> read_file(string filename)
 {
     ifstream file(filename, ios::binary | ios::ate);
     streamsize size = file.tellg();
@@ -54,12 +70,14 @@ static void analyse_file(string filename, QSize framesize, int nr_cycles)
     qint64 t_total_min = 9223372036854775807;
     qint64 t_total_avg = 0;
 
-
     zbar::ImageScanner image_scanner;
 
     // only look for QR codes: disable all + enable QR
     image_scanner.set_config((zbar::zbar_symbol_type_e) 0, zbar::ZBAR_CFG_ENABLE, 0);
     image_scanner.set_config(zbar::ZBAR_QRCODE, zbar::ZBAR_CFG_ENABLE, 1);
+
+    QRect search_area;
+    bool search_area_found = false;
 
     for (int i = 0; i < nr_cycles; ++i) {
 
@@ -72,34 +90,60 @@ static void analyse_file(string filename, QSize framesize, int nr_cycles)
 
             QImage qimage((unsigned char*) data, framesize.width(), framesize.height(), QImage::Format_RGB888);
 
+            //qimage.save(QString("original_image_%1.png").arg(i));
+
+            QImage qimage_cropped;
+            if (search_area_found) {
+                //crop image to search area
+                qimage_cropped = qimage.copy(search_area);
+            }
+            else {
+                qimage_cropped = qimage;
+            }
+
             quint64 t_now = timer.nsecsElapsed();
             t1 = t_now - t0;
             t0 = t_now;
 
-            // Scanner can only convert Y800 or GRAY image formats
-            QImage qimage_converted = qimage.convertToFormat(QImage::Format_Grayscale8);
-            zbar::Image image_converted(qimage_converted.width(), qimage_converted.height(), "Y800", qimage_converted.bits(), qimage_converted.byteCount());
+            // Scanner can only scan Y800 (=grayscale, 1 byte/px) or GRAY image formats
+            QImage qimage_converted_and_cropped = qimage_cropped.convertToFormat(QImage::Format_Grayscale8);
+            zbar::Image image(qimage_converted_and_cropped.width(), qimage_converted_and_cropped.height(), "Y800",
+                                        qimage_converted_and_cropped.bits(), qimage_converted_and_cropped.byteCount());
+
+
+            //QImage qimage_from_zbar((unsigned char*) image.get_data(), image.get_width(), image.get_height(), QImage::Format_Grayscale8);
+            //qimage_from_zbar.save(QString("cropped_image_%1_grayscale.png").arg(i));
 
             t_now = timer.nsecsElapsed();
             t2 = t_now - t0;
             t0 = t_now;
 
-            int result  = image_scanner.scan(image_converted);
+            int result  = image_scanner.scan(image);
             if (result < 0) cout << "Image scanning failed!!" << endl;
 
             t_now = timer.nsecsElapsed();
             t3 = t_now - t0;
             t0 = t_now;
 
-            zbar::SymbolSet results = image_converted.get_symbols();
+            zbar::SymbolSet results = image.get_symbols();
 
             t_now = timer.nsecsElapsed();
             t4 = t_now - t0;
             t0 = t_now;
 
             if (results.get_size() > 0) {
-                std::string symbol_data = results.symbol_begin()->get_data();
+                zbar::SymbolIterator symbol = results.symbol_begin();
+                std::string symbol_data = symbol->get_data();
                 cout << "QR symbol found: " << symbol_data << endl;
+                QRect symbol_area = get_area(*symbol);
+                cout << QString("Symbol_area: top left (%1, %2), size: %3x%4")
+                        .arg(symbol_area.left()).arg(symbol_area.top()).arg(symbol_area.width()).arg(symbol_area.height()).toStdString() << endl;
+                if (!search_area_found) {
+                    cout << "Applying new search area" << endl;
+                    QRect symbol_area_with_margins = symbol_area.marginsAdded({50, 50, 50, 50});
+                    search_area = symbol_area_with_margins;
+                    search_area_found = true;
+                }
             }
             else {
                 cout << "No QR symbol found" << endl;
